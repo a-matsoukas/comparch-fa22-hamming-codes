ffdf977e6f183478ab79a140e7da09a9b516d77c
lila <lsmith2@olin.edu>
2022-12-16 10:27:32 -0500

    Add comments to code and update README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # comparch-fa22-hamming-codes
+
+The goal of our project is to implement Hamming(7, 4) (7 bits per message, 4 of them are data bits) error correction in simulation. To see the simulations, try running `make test_hamming_7_4_encoder` and `make test_hamming_7_4_encoder`. 
+
+Please see our website for this project [here](https://a-matsoukas.github.io/comparch-fa22-hamming-codes/).
--- hdl/main.sv
+++ hdl/main.sv
@@ -3,12 +3,17 @@
 
 module main(data, error_vector, tx_msg_bar, rx_msg_bar, ec_data_bar);
 
+// P is number of parity bits for future implementation.
 parameter P = 3;
 
+// We receive these inputs from switches on the bread board.
 input wire [2**P - P - 2:0] data;
 input wire [2**P - 2:0] error_vector;
+
 logic [2**P - P - 2:0] ec_data;
 logic [2**P - 2:0] tx_msg, rx_msg;
+
+// These outputs are used to light up LEDs and verify it works.
 output logic [2**P - P - 2:0] ec_data_bar;
 output logic [2**P - 2:0] tx_msg_bar, rx_msg_bar;
 
@@ -16,10 +21,15 @@ output logic [2**P - 2:0] tx_msg_bar, rx_msg_bar;
 hamming_7_4_encoder #(.P(P)) encoder(data, tx_msg);
 hamming_7_4_decoder #(.P(P)) decoder(rx_msg, ec_data);
 
+// This block combines transmitted message with an error vector to simulate a
+// noisy channel. This produced the received message.
 always_comb begin : bit_error
     rx_msg = tx_msg ^ ~error_vector; 
 end
 
+// We want to power LEDs from a power rail due to pin current constraints, so
+// the cathode is connected to our specific pin. To do this, we NOT the signals
+// so that HIGH is LED off and LOW is LED on.
 always_comb begin : outputs
     tx_msg_bar = ~tx_msg;
     rx_msg_bar = ~rx_msg;
